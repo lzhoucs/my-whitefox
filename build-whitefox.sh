8ba58ab026ea4b04e7bf510198ab4c8f31cca7ce
Liang Zhou <lzhoucs@gmail.com>
2018-12-24 13:35:08 -0600

    Add fast_mode. Move build artifact to current dir.
    Moving artifact to current dir provides the ability to source control artifacts.

--- build-whitefox.sh
+++ build-whitefox.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 
 # A wrapper over the target build command 'whitefox.bash'
+BuildDir=darwin18.MyWhiteFoxBuild.gcc.ninja
+fast_mode=false
+
+while getopts ":f" opt; do
+    case $opt in
+        f)
+            fast_mode=true
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            exit 1
+            ;;
+    esac
+done
 
 echo ''
 echo '============ Start building my whitefox keyboard ============='
@@ -8,8 +22,16 @@ echo ''
 
 cd ../kiibohd-controller/Keyboards
 
-pipenv install
+if [ "$fast_mode" = false ] ; then
+    echo 'fast_mode is off by default. Install dependencies...'
+    pipenv install
+else
+    echo 'fast_mode is on. Skip install dependencies'
+fi
+
 pipenv run ./whitefox.bash
+
+mv ${BuildDir}/kiibohd.dfu.bin ../../my-whiteFox-TrueFox/
 
 echo ''
 echo '============ Finished building my whitefox keyboard ============='
--- flash-whitefox.sh
+++ flash-whitefox.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
 # A wrapper that calls dfu-util with the right kiibohd.dfu.bin file built with './build-whitefox.sh'
-BuildDir=darwin18.MyWhiteFoxBuild.gcc.ninja
 
 echo ''
 echo '============ Start flashing my whitefox keyboard ============='
 echo ''
 
-dfu-util -D ../kiibohd-controller/Keyboards/${BuildDir}/kiibohd.dfu.bin
+dfu-util -D ./kiibohd.dfu.bin
 
 echo ''
 echo '============ Finished flashing my whitefox keyboard ============='
