4d5ad5a1937e75f998462f5a0824dc35c907ace1
Liang Zhou <lzhoucs@gmail.com>
2018-12-24 13:34:47 -0600

    rename flash script.

--- flash-whitefox.sh
+++ flash-whitefox.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
 # A wrapper that calls dfu-util with the right kiibohd.dfu.bin file built with './build-whitefox.sh'
+BuildDir=darwin18.MyWhiteFoxBuild.gcc.ninja
+
 echo ''
 echo '============ Start flashing my whitefox keyboard ============='
 echo ''
 
-dfu-util -D ../kiibohd-controller/Keyboards/darwin18.MyWhiteFoxBuild.gcc.ninja/kiibohd.dfu.bin
+dfu-util -D ../kiibohd-controller/Keyboards/${BuildDir}/kiibohd.dfu.bin
 
 echo ''
 echo '============ Finished flashing my whitefox keyboard ============='
