c081034c3ebf93223a26f0ca9887135effadd848
Liang Zhou <lzhoucs@gmail.com>
2020-03-05 00:01:54 -0600

    comment out everything before customize

--- qmk-keymaps/rules.mk
+++ qmk-keymaps/rules.mk
@@ -1,5 +1,5 @@
 # MCU name
-MCU = MK20DX256
+# MCU = MK20DX256
 
 # Linker script to use
 # - it should exist either in <chibios>/os/common/ports/ARMCMx/compilers/GCC/ld/
@@ -11,7 +11,7 @@ MCU = MK20DX256
 #   - MK20DX256 for Teensy 3.1 and 3.2
 #   - MK20DX128BLDR4 for Infinity with Kiibohd bootloader
 #   - MK20DX256BLDR8 for Infinity ErgoDox with Kiibohd bootloader
-MCU_LDSCRIPT = MK20DX256BLDR8
+# MCU_LDSCRIPT = MK20DX256BLDR8
 
 # Board: it should exist either in <chibios>/os/hal/boards/
 #  or <this_dir>/boards
@@ -20,37 +20,25 @@ MCU_LDSCRIPT = MK20DX256BLDR8
 #   - PJRC_TEENSY_3 for Teensy 3.0
 #   - PJRC_TEENSY_3_1 for Teensy 3.1 or 3.2
 #   - MCHCK_K20 for Infinity KB
-BOARD = IC_TEENSY_3_1
+# BOARD = IC_TEENSY_3_1
 
-DFU_ARGS = -d 1c11:b007
-DFU_SUFFIX_ARGS = -p b007 -v 1c11
+# DFU_ARGS = -d 1c11:b007
+# DFU_SUFFIX_ARGS = -p b007 -v 1c11
 
-BOOTLOADER = dfu
+# BOOTLOADER = dfu
 
 # Build Options
 #   comment out to disable the options.
 #
-BOOTMAGIC_ENABLE = yes	# Virtual DIP switch configuration
+# BOOTMAGIC_ENABLE = yes	# Virtual DIP switch configuration
 ## (Note that for BOOTMAGIC on Teensy LC you have to use a custom .ld script.)
-MOUSEKEY_ENABLE = yes	# Mouse keys
-EXTRAKEY_ENABLE = yes	# Audio control and System control
+# MOUSEKEY_ENABLE = yes	# Mouse keys
+# EXTRAKEY_ENABLE = yes	# Audio control and System control
 #CONSOLE_ENABLE = yes	# Console for debug
-COMMAND_ENABLE = yes    # Commands for debug and configuration
+# COMMAND_ENABLE = yes    # Commands for debug and configuration
 #SLEEP_LED_ENABLE = yes  # Breathing sleep LED during USB suspend
-NKRO_ENABLE = yes	    # USB Nkey Rollover
-CUSTOM_MATRIX = yes # Custom matrix file
-BACKLIGHT_ENABLE = yes
-BACKLIGHT_DRIVER = custom
-VISUALIZER_ENABLE = yes
-
-# project specific files
-SRC = matrix.c \
-      led.c
-
-LED_DRIVER = is31fl3731c
-LED_WIDTH = 16
-LED_HEIGHT = 5
-
-
-# Enter lower-power sleep mode when on the ChibiOS idle thread
-OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
+# NKRO_ENABLE = yes	    # USB Nkey Rollover
+# CUSTOM_MATRIX = yes # Custom matrix file
+# BACKLIGHT_ENABLE = yes
+# BACKLIGHT_DRIVER = custom
+# VISUALIZER_ENABLE = yes
