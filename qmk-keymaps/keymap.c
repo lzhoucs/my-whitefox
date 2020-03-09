55c02b95fbc4646f907baa824ff75051dd518f83
Liang Zhou <lzhoucs@gmail.com>
2020-03-08 23:44:46 -0500

    Add basic media keys.

--- qmk-keymaps/keymap.c
+++ qmk-keymaps/keymap.c
@@ -58,15 +58,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┼───┤
    * │  NO  │NO │NO │NO │NO │NO │NO │NO │NO │NO │NO │NO │  NO    │NO │
    * ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────┬───┼───┤
-   * │   NO   │NO │NO │NO │NO │NO │NO │NO │NO │NO │NO │  NO  │NO |NO │
+   * │   NO   │NO │NO │NO │NO │NO │NO │NO │NO │NO │NO │ PLAY │VLU|NO │
    * ├────┬───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬─┬───┼───┼───┤
-   * │ NO | NO │ NO │        NO              │ NO │ NO │ |NO │NO |NO |
+   * │ NO | NO │ NO │        NO              │ NO │ NO │ |PRV│VLD|NXT|
    * └────┴────┴────┴────────────────────────┴────┴────┘ └───┴───┴───┘
    */
   [L_FN] = LAYOUT_truefox(
     KC_NO,	KC_F1,	KC_F2,	KC_F3,	KC_F4,	KC_F5,	KC_F6,	KC_F7,	KC_F8,	KC_F9,	KC_F10,	KC_F11,	KC_F12,	KC_NO,	KC_NO,	RESET,
     KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,		KC_NO,		KC_NO,		KC_NO,	KC_NO,
     KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,		KC_NO,		KC_NO,		KC_NO,
-    KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,		KC_NO,		KC_NO,		KC_NO,
-    KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO),
+    KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,		KC_MPLY,	KC_VOLU,	KC_NO,
+    KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_MPRV,	KC_VOLD,	KC_MNXT),
 };
