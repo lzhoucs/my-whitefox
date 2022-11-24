6cbb92d78df9950ef2257263e02d637fee275342
Liang Zhou <lzhoucs@gmail.com>
2022-11-24 03:14:12 -0600

    qmk workflow updates after 3 years.

---
+++ bin/my_whitefox.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env zsh
+KBD=$(dirname "$0")/..
+source $KBD/venv/bin/activate
+
+if [ "$1" = "compile" ]; then
+	qmk compile
+elif [ "$1" = "flash" ]; then
+	qmk flash
+else
+	echo "Specify either compile or flash as argument"
+fi
