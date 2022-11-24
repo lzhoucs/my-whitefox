9d5d3330e0d970600693678259dad7bb6e6cfd3d
Liang Zhou <lzhoucs@gmail.com>
2022-11-24 17:17:09 -0600

    update RESET key.
    RESET key seems disappeared (undocumented) even though it's working. Replace it with
    the new one QK_BOOT.

--- qmk-keymaps/keymap.c
+++ qmk-keymaps/keymap.c
@@ -37,6 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * NXL = Next Layer
    * ANY = Any Key
    * NO = Not Defined yet
+   * FLS = Put the keyboard into bootloader mode for flashing
    */
 
   /* Base Layer
@@ -78,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * └────┴────┴────┴────────────────────────┴────┴────┘ └───┴───┴───┘
    */
   [L_FN] = LAYOUT_truefox(
-    KC_NO,	KC_F1,	KC_F2,	KC_F3,	KC_F4,	KC_F5,	KC_F6,	KC_F7,	KC_F8,	KC_F9,	KC_F10,	KC_F11,	KC_F12,	KC_NO,	KC_NO,	RESET,
+    KC_NO,	KC_F1,	KC_F2,	KC_F3,	KC_F4,	KC_F5,	KC_F6,	KC_F7,	KC_F8,	KC_F9,	KC_F10,	KC_F11,	KC_F12,	KC_NO,	KC_NO,	QK_BOOT,
     KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,		KC_NO,		KC_NO,		KC_NO,	KC_NO,
     KC_NO,	KC_NO,	KC_NO,	DEBUG,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	BL_TOGG,KC_NO,		KC_NO,		KC_NO,		KC_NO,
     KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,		KC_MPLY,	KC_VOLU,	KC_NO,
